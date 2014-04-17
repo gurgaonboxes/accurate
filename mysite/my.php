50a4edd60a98f67879cd5e7e3ba3703088c879d8
hemant22021 <hk_2021@live.in>
2014-04-17 15:12:31 +0530

    made by hemant

---
+++ mysite/my.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>My website</title></head>
+<body>
+<?php
+print "this is my wbsite";?>
+<p>
+<h1>helloo... this website is just for the testing purpose..</h1></p>
+<h2><marquee behavior="alternate" bgcolor=red >Made by Hemant kumar</marquee></h2>
+
+</body>
+</html>
